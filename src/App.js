ac16c68b17bb9063c49a7215dd1b64df3dbbbc84
gasmi-rafik <gasmi.rafik.rg@gmail.com>
2022-04-28 11:57:33 +0200

    nasri second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Nasri from './Nasri';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Nasri/>
         <a
           className="App-link"
           href="https://reactjs.org"
